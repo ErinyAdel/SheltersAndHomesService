84d70c2df019929a6a5bfd1433bfadedced6b4eb
Eriny Adel <renyadel7@gmail.com>
2025-05-13 23:12:11 +0300

    Add The Template Files & Implement Orphanages Dynamically

---
+++ SheltersAndHomesService/Controllers/SectionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SheltersAndHomesService.Models;
+
+namespace SheltersAndHomesService.Controllers
+{
+    public class SectionController : Controller
+    {
+        public IActionResult Orphanage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OrphanageDetails(OrphanageDetailsModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ SheltersAndHomesService/Models/OrphanageDetailsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SheltersAndHomesService.Models
+{
+    public class OrphanageDetailsModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
